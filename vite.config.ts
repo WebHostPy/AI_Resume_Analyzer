e25f130cfa3e844befa8a7dadf1d4b2a21077225
WebHostPy <154913437+WebHostPy@users.noreply.github.com>
2025-12-07 00:53:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    react(),       // React plugin
-    tsconfigPaths() // TS path alias support
-  ],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
